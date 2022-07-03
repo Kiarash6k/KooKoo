03d59d6f64109015538b1d97c0545aaeed39ea78
Kiarash <kowsarikiarash@gmail.com>
2022-07-04 01:52:53 +0430

    Design HomePage

--- kookoo/lib/HomeScreen.dart
+++ kookoo/lib/HomeScreen.dart
@@ -22,10 +22,7 @@ class _HomeScreeenState extends State<HomeScreeen> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: new ThemeData(
-            primaryColor:
-                Color.fromARGB(255, 40, 90, 82), // Your app THEME-COLOR
-          ),
+      
       home:Scaffold(
         bottomNavigationBar: Diamond(
           selectedColor: Color.fromARGB(255, 218, 218, 219),
@@ -51,8 +48,136 @@ class _HomeScreeenState extends State<HomeScreeen> {
           Image.asset("assets/images/Ham.png",color: Colors.white,scale:15,),
           Padding(padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*.75),
           child:Image.asset("assets/images/kookoo.png",color: Colors.white,scale:10,))
-        ]))
-       
+        ])),
+        SizedBox(height: MediaQuery.of(context).size.height/20,),
+        Row(
+          children: [
+            Container(
+              margin: EdgeInsets.only( top: MediaQuery.of(context).size.height/8, left: MediaQuery.of(context).size.width*0.07),
+              width: MediaQuery.of(context).size.width/2.5,
+              height: MediaQuery.of(context).size.height/6,
+            child:
+            Column(
+              children: [
+                SizedBox(height: MediaQuery.of(context).size.height/50,),
+                Icon(Icons.alarm_on_outlined,
+                color: Colors.black,size: MediaQuery.of(context).size.width/12,),
+                SizedBox(height: MediaQuery.of(context).size.height/95,),
+                Text("0",textAlign: TextAlign.center,
+                style: TextStyle(fontSize: MediaQuery.of(context).size.height/30,color: Color.fromARGB(255, 0, 0, 0),
+                fontWeight: FontWeight.bold,shadows: [
+                  Shadow(
+                    color: Color.fromARGB(228, 0, 0, 0),
+                    blurRadius: 20,
+                    offset: Offset(5, 1),
+                  ),
+                ],),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height/80,),
+                Text("Total Alarms",textAlign: TextAlign.center,
+                style: TextStyle(fontSize: MediaQuery.of(context).size.height/45,color: Color.fromARGB(255, 0, 0, 0)),),
+                
+                
+              ],
+            ),
+            decoration: BoxDecoration(
+              boxShadow: [BoxShadow(
+                color: Colors.black,
+                blurRadius: 10,
+                offset: Offset(0, 10),
+              ),],
+              borderRadius: BorderRadius.circular(20),
+              color: Color.fromARGB(255,193,222,220),
+            ),
+            ),
+            Container(
+              margin: EdgeInsets.only( top: MediaQuery.of(context).size.height/8, left: MediaQuery.of(context).size.width*0.05),
+              width: MediaQuery.of(context).size.width/2.5,
+              height: MediaQuery.of(context).size.height/6,
+            child:Column(
+              children: [
+                SizedBox(height: MediaQuery.of(context).size.height/50,),
+                Icon(Icons.analytics,
+                color: Colors.black,size: MediaQuery.of(context).size.width/12,),
+                SizedBox(height: MediaQuery.of(context).size.height/95,),
+                Text("LINKUSDTP",textAlign: TextAlign.center,
+                style: TextStyle(fontSize: MediaQuery.of(context).size.height/40,color: Color.fromARGB(255, 0, 0, 0),
+                fontWeight: FontWeight.bold,shadows: [
+                  Shadow(
+                    color: Color.fromARGB(228, 0, 0, 0),
+                    blurRadius: 50,
+                    offset: Offset(5, 1),
+                  ),
+                ],),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height/80,),
+                Text("Currency",textAlign: TextAlign.center,
+                style: TextStyle(fontSize: MediaQuery.of(context).size.height/45,color: Color.fromARGB(255, 0, 0, 0)),),
+                
+                
+              ],
+            ),
+            decoration: BoxDecoration(
+              boxShadow: [BoxShadow(
+                color: Colors.black,
+                blurRadius: 10,
+                offset: Offset(0, 10),
+              ),],
+              borderRadius: BorderRadius.circular(20),
+              color: Color.fromARGB(255,231,222,241),
+            ),
+            ),
+          ],
+        ),
+         Container(
+              margin: EdgeInsets.only( top: MediaQuery.of(context).size.height*0.05, left: MediaQuery.of(context).size.width*0.07,right: MediaQuery.of(context).size.width*0.07),
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height/6,
+            child:
+            Column(
+              children: [
+                SizedBox(height: MediaQuery.of(context).size.height/70,),
+                Icon(Icons.attach_money,
+                color: Colors.black,size: MediaQuery.of(context).size.width/12,),
+                //SizedBox(height: MediaQuery.of(context).size.height/110,),
+                Text("6.226",textAlign: TextAlign.center,
+                style: TextStyle(fontSize: MediaQuery.of(context).size.height/30,color: Color.fromARGB(255, 0, 0, 0),
+                fontWeight: FontWeight.bold,shadows: [
+                  Shadow(
+                    color: Color.fromARGB(228, 0, 0, 0),
+                    blurRadius: 50,
+                    offset: Offset(5, 1),
+                  ),
+                ],),
+                ),
+                //SizedBox(height: MediaQuery.of(context).size.height/120,),
+                Text("+18.8  +0.03%",textAlign: TextAlign.center,
+                style: TextStyle(fontSize: MediaQuery.of(context).size.height/60,color: Color.fromARGB(255, 52, 131, 0),
+                fontWeight: FontWeight.bold,shadows: [
+                  Shadow(
+                    color: Color.fromARGB(228, 0, 0, 0),
+                    blurRadius: 20,
+                    offset: Offset(5, 1),
+                  ),
+                ],),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height/80,),
+                Text("Current Price",textAlign: TextAlign.center,
+                style: TextStyle(fontSize: MediaQuery.of(context).size.height/45,color: Color.fromARGB(255, 0, 0, 0)),),
+                
+                
+              ],
+            ),
+            decoration: BoxDecoration(
+              boxShadow: [BoxShadow(
+                color: Colors.black,
+                blurRadius: 10,
+                offset: Offset(0, 10),
+              ),],
+              borderRadius: BorderRadius.circular(20),
+              color: Color.fromARGB(255,242,239,232),
+            ),
+            ), 
       ]),
     ),);
   }
